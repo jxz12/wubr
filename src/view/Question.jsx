d9f76365dec830bf5e0f675ef48b119ff94923cf
Jonathan Zheng <jxz12@ic.ac.uk>
2025-01-19 18:25:38 +0000

    convert number to accent pinyin

--- src/view/Question.jsx
+++ src/view/Question.jsx
@@ -6,7 +6,7 @@ export default function Question({ question, answer, characterSet, inputMethod }
           key={i}
           characters={word[characterSet]}
           spelling={word[inputMethod]}
-          meaning={word.meanings}
+          meanings={word.meanings}
         />
       ))}
     </div>
@@ -25,10 +25,38 @@ function Word({ characters, spelling, meanings }) {
     <span className="ci">
       {charArray.map((_, i) => (
         <span key={i} className="zi">
-          <div className="spelling">{spellArray[i]}</div>
+          <div className="spelling">{accentPinyin(spellArray[i])}</div>
           <div className="character">{charArray[i]}</div>
         </span>
       ))}
     </span>
   );
+}
+
+function accentPinyin(pinyin) {
+  const tone = Number(pinyin[pinyin.length - 1]);
+  const vowelStartIdx = pinyin.split("").findIndex(x => "aeiouv".includes(x));
+  const vowelEndIdx = pinyin.split("").findLastIndex(x => "aeiouv".includes(x));
+
+  const vowelAccentIdx = "iuv".includes(pinyin[vowelStartIdx])
+    ? Math.min(vowelStartIdx + 1, vowelEndIdx)
+    : vowelStartIdx;
+  const vowelAccent = pinyin[vowelAccentIdx];
+
+  const correctedAccent = {
+    a: ["ā", "á", "ǎ", "à", "a"],
+    A: ["Ā", "Á", "Ǎ", "À", "A"],
+    e: ["ē", "é", "ě", "è", "e"],
+    E: ["Ē", "É", "Ě", "È", "E"],
+    i: ["ī", "í", "ǐ", "ì", "i"],
+    I: ["Ī", "Í", "Ǐ", "Ì", "I"],
+    o: ["ō", "ó", "ǒ", "ò", "o"],
+    O: ["Ō", "Ó", "Ǒ", "Ò", "o"],
+    u: ["ū", "ú", "ǔ", "ù", "u"],
+    U: ["Ū", "Ú", "Ǔ", "Ù", "u"],
+    v: ["ǖ", "ǘ", "ǚ", "ǜ", "ü"],
+    V: ["Ǖ", "Ǘ", "Ǚ", "Ǜ", "Ü"],
+  }[vowelAccent][tone - 1]
+
+  return `${pinyin.substring(0, vowelStartIdx)}${pinyin.substring(vowelStartIdx, vowelAccentIdx)}${correctedAccent}${pinyin.substring(vowelAccentIdx+1, pinyin.length-1)}`;
 }
